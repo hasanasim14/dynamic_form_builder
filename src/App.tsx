6b8f580b861b06bf555ce356ee5542081e66e1d1
DESKTOP-4AG4OF3\pc <hasan.asim14@gmail.com>
2025-01-24 02:59:46 +0500

    Removed extra code.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FormBuilder from "./assets/components/FormBuilder";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen w-full p-4 bg-gray-50">
+    <main className="flex flex-col items-center justify-start min-h-screen w-full p-4 bg-[#f0ebf8]">
       <h1 className="text-4xl font-bold text-center text-gray-800 mb-8 mt-12 w-full">
         Dynamic Form Builder
       </h1>
